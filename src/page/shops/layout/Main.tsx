a92919aa8ccd6d688f3af6fdd5acaaed86e0471d
MUKEBUDAVID <davidmukebu@gmail.com>
2025-05-14 05:31:41 +0100

    fin de la partie filterAndSearch de la page shop

--- src/page/shops/layout/Main.tsx
+++ src/page/shops/layout/Main.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router"
 function Main() {
   return (
     <main>
+
   <section className="publicite">
   <span className="shopPage">Shop</span>
   <p className="shopNav">
@@ -13,7 +14,41 @@ function Main() {
     <span title="page actuel">Shop</span></p>
   
   </section>
-  <section></section>
+
+  <section className="filtreAndSearch">
+
+    <div className="filtre">
+
+      <div className="filtreIcon">
+
+         <div> 
+          <img src="/img/system-uicons_filtering.png" alt="filter_icon"  /> 
+         <span>Filter</span>
+         </div>
+       <img src="/img/ci_grid-big-round.png" alt="filter_icon"  /> 
+       <img src="/img/bi_view-list.png" alt="filter_icon"  />
+
+      </div>
+
+    <span className="separateur"></span>
+
+
+    <p className="pageView">
+      {`Showing ${1}–${16} of ${32} results`}
+    </p>
+      
+    </div>
+
+
+
+
+    <div className="searchBar">
+       <input type="text" placeholder="recherche"  />
+      <button type="button"> <img src="/img/akar-icons_search.png" alt="searchIcon"  /></button>
+    </div>
+
+  </section>
+
   <section></section>
   <section></section>
         
